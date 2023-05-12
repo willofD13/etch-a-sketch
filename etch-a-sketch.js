f960529a07dfe3af9263edd72ce06b56a2c7f535
Leonidas Tsiakas <leonidastsiakas13@gmail.com>
2023-05-12 21:17:55 +0300

    Add a class to each div

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -2,6 +2,7 @@ const container = document.querySelector('.container')
 
 for (i=0; i<256; i++) {
     const div = document.createElement('div')
+    div.classList.add('square')
     div.style.width= '48px'
     div.style.height = '48px'
     div.style.border = '1px solid black'
