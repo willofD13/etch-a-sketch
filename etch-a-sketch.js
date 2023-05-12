dfafe587d9b39409515ccf4f1b45da375aab89a6
Leonidas Tsiakas <leonidastsiakas13@gmail.com>
2023-05-12 21:19:25 +0300

    Change div's background color when mouse is over it

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -8,3 +8,9 @@ for (i=0; i<256; i++) {
     div.style.border = '1px solid black'
     container.appendChild(div)
 }
+const divS = document.querySelectorAll('.square')
+divS.forEach(element => {
+    element.addEventListener('mouseover', () => {
+        element.classList.add('trail');
+    });
+});
