ad9f40a2d9e0c1e155da7510c7d0717e7f197b8c
Leonidas Tsiakas <leonidastsiakas13@gmail.com>
2023-05-12 14:53:58 +0300

    Create four main divs

---
+++ etch-a-sketch.js
@@ -0,0 +1,17 @@
+const container = document.querySelector('.container');
+
+const div1 = document.createElement('div');
+div1.classList.add('square1')
+container.appendChild('div1')
+
+const div2 = document.createElement('div');
+div2.classList.add('square2')
+container.appendChild('div2')
+
+const div3 = document.createElement('div');
+div3.classList.add('square3')
+container.appendChild('div3')
+
+const div4 = document.createElement('div');
+div4.classList.add('square4')
+container.appendChild('div4')
